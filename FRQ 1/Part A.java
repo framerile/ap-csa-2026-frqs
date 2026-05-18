bc3fbc7c57f105a243d119b7e543d7fbad671193
framerile <107064389+framerile@users.noreply.github.com>
2026-05-18 11:58:10 -0400

    Update Part A.java

--- FRQ 1/Part A.java
+++ FRQ 1/Part A.java
@@ -1,1 +1,9 @@
+public Account(String requestedName){
+    String saved = requestedName;
 
+    for(int i = 1; !isAvailable(saved); i++){
+      saved = requestedName + i;
+    }
+
+    this.username = saved;
+}
