5e2f93c0f3f040b8b3f8a15d4a5d097a95eb7c74
framerile <107064389+framerile@users.noreply.github.com>
2026-05-18 20:06:21 -0400

    Created Bottle.java

---
+++ FRQ 2/Bottle.java
@@ -0,0 +1,20 @@
+public class Bottle{
+    private double capacity;
+    private double filled;
+  
+    public Bottle(double capacity){
+        this.capacity = capacity;
+        filled = capacity;
+    }
+
+    public double updateAmount(double taken){
+        if(filled - taken < capacity / 4){
+            filled = capacity;
+            return capacity;
+        }
+
+        fill -= taken;
+        return fill;
+    }
+  
+}
